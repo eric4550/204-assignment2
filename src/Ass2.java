5f91f1fdff3e25e0ca97a1ecf3954cb8c735bc19
Erickson <de19803@essex.ac.uk>
2020-03-07 18:21:25 +0000

    created ass2

---
+++ src/Ass2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ass2 {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        String line = "";
+
+        System.out.println("Reg number 1909766");
+        System.out.println("Welcome to Parker's expression evaluation program. Please type an expression: ");
+        line = stdin.nextLine();
+        System.out.println(line);
+    }
+}
